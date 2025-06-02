2bbd67b51267f141bcfa06e9ff59d882f1332122
GaneshR2766 <k.raja671978@gmail.com>
2025-06-02 23:52:01 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/cloud-doc/',
+  base: '/cloud-doc/',  // For GitHub Pages
   plugins: [react()],
+  define: {
+    'process.env': {}   // Fix for using env vars in browser code
+  }
 })
